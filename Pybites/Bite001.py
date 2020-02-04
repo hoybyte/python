5a34ef9fb5606b2d56cd8fa46a93d279fe922e1c
Chris Krygier <hoybyte@gmail.com>
2020-02-03 22:17:42 -0500

    Added Pybites examples

---
+++ Pybites/Bite001.py
@@ -0,0 +1,4 @@
+# Write a function that can sum up numbers:
+#     It should receive a list of n numbers.
+#     If no argument is provided, return sum of numbers 1..100.
+#     Look closely to the type of the function's default argument ...
---
+++ ZeroToMastery/findduplicateinlist.py
@@ -0,0 +1,3 @@
+#Exercise: Check for duplicates in list:
+# Show duplicates after you found them by using print
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
