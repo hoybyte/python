91312648cbe20c5b37ba9053507ed07cb89f124c
Chris Krygier <hoybyte@gmail.com>
2020-02-03 21:38:56 -0500

    Added Pybites examples

--- Pybites/Bite100.py
+++
@@ -1,9 +0,0 @@
-MIN_DRIVE_AGE = 16
-NAME = input("What is your name? ")
-AGE = int(input("How old are you? "))
-
-if AGE > MIN_DRIVE_AGE :
-    print(f"{NAME} is not allowed to drive")
-
-else:
-    print(f"{NAME} is allowed to drive")
---
+++ ZeroToMastery/trickycounter.py
@@ -0,0 +1,14 @@
+my_list = [1,2,3,4,5,6,7,8,9,10]
+
+counter = 0
+
+for item in my_list:
+    counter = counter + item
+
+print(counter)
+
+number = 0
+
+for i in range(0,100):
+    number = number + i
+print(number)
