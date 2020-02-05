c564d0e89036615bd6b7ee62c3da90678ecc3fe0
Chris Krygier <hoybyte@gmail.com>
2020-02-05 15:56:15 -0500

    Added tesla function training file.

---
+++ ZeroToMastery/teslafunctiontraining.py
@@ -0,0 +1,10 @@
+def checkDriverAge(age=0):
+    age = (input("What is your age?: ")
+    if int(age) < 18:
+        print("Sorry, you are too young to drive this car. Powering Off")
+    elif int(age) > 18:
+        print("Powering On. Enjoy the ride!")
+    elif int(age) == 18:
+        print("Congratulations on your first year of driving. Enjoy the ride!")
+
+checkDriverAge()
