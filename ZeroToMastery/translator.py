8e6e8ef3a31080d1dbb76cdae7a78041fb68d302
Krygier <krygich@richmond.k12.ga.us>
2020-05-04 20:00:08 -0400

    Added Translate module to translator.py

--- ZeroToMastery/translator.py
+++ ZeroToMastery/translator.py
@@ -1,10 +1,11 @@
-#TODO Request file location input from user
 #TODO Read inputted File Location
 #TODO Translate to language
 #TODO Determine which langaguge user wants
 
 from pathlib import Path
+from translate import Translator
 
+translator = Translator(to_lang='es')
 file_path = Path(input("What file do you want translated?"))
 
 language = input(f"What language do you want the file, {file_path}, to be translated to:  ")
@@ -12,10 +13,12 @@ language = input(f"What language do you want the file, {file_path}, to be transl
 print(file_path)
 print(language)
 
-try:
-    with open(file_path, mode='r') as my_file:
-        print(my_file.readlines())
-except FileNotFoundError as err:
-    print('File does not exist')
-    raise
+# try:
+#     with open(file_path, mode='r') as my_file:
+#         print(my_file.readlines())
+# except FileNotFoundError as err:
+#     print('File does not exist')
+#     raise
 
+translation = translator.translate("This is my Name")
+print(translation)
