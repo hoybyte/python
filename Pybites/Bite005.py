f3d2af00389d8ee334db8b59660beeb290fab44e
Krygier <krygich@richmond.k12.ga.us>
2020-09-11 17:39:46 -0400

    Answered Pybite005 and uploaded solution

--- Pybites/Bite005.py
+++ Pybites/Bite005.py
@@ -1,12 +1,26 @@
-    # In this bite you will work with a list of names.
-    # First you will write a function to take out duplicates and title case them.
-    # Then you will sort the list in alphabetical descending order by surname and lastly determine what the shortest first name is. For this exercise you can assume there is always one name and one surname.
-    # With some handy Python builtins you can write this in a pretty concise way. Get it sorted :)
-
-
-
-
 NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
          'julian sequeira', 'sandra bullock', 'keanu reeves',
          'julbob pybites', 'bob belderbos', 'julian sequeira',
          'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
+
+def dedup_and_title_case_names(names):
+    """ Using Set to dedupe and Title() to title case"""
+    return list(set(name.title() for name in names))
+
+def sort_by_surname_desc(names):
+    names = dedup_and_title_case_names(names)
+    return [" ".join(x) for x in sorted([name.split() for name in names], 
+                                        key=lambda x: x[1], reverse=True)]
+
+def shortest_first_name(names):
+    names = dedup_and_title_case_names(names)
+    return sorted([name.split()[0] for name in names], key=len)[0]
+
+def shortest_last_name(names):
+    names = dedup_and_title_case_names(names)
+    return sorted([name.split()[1] for name in names], key=len)[0]
+
+dedup_and_title_case_names(NAMES)
+sort_by_surname_desc(NAMES)
+shortest_first_name(NAMES)
+shortest_last_name(NAMES)
