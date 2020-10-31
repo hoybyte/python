0190e3b59acfa323e31ec45229b2acbf9a803f9b
Krygier <krygich@richmond.k12.ga.us>
2020-10-30 21:16:56 -0400

    Changed API to Species of PokeAPI and added Random

--- Programs/Pokemon/api.py
+++ Programs/Pokemon/api.py
@@ -7,10 +7,14 @@ Pokemon = collections.namedtuple('Pokemon', 'name, weight')
 
 
 def find_pokemon_from_name(id: str) -> List[Pokemon]:
-    url = f'https://pokeapi.co/api/v2/pokemon/{id}'
+    url = f'https://pokeapi.co/api/v2/pokemon-species/{id}'
 
     resp = requests.get(url)
     resp.raise_for_status()
 
     results = resp.json()
+
+    information = results['flavor_text_entries']
+
     print(results['name'])
+    print(information[0])
--- Programs/Pokemon/program.py
+++ Programs/Pokemon/program.py
@@ -1,12 +1,12 @@
 from api import find_pokemon_from_name
+import random
 
 
 def main():
     # Insert Pokemon Number in Function
-    poke = input("What pokemon do you want? ")
+    poke = random.randrange(1, 151, 1)
+    # poke = input("What pokemon do you want? ")
     find_pokemon_from_name(poke)
-    print(find_pokemon_from_name(poke))
-    pass
 
 
 if __name__ == "__main__":
